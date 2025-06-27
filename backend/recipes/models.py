4ec8dfc3a0ecd15ed279737db30c903ceedcfa7c
udazzZZ <pirogova.dv96@mail.ru>
2025-06-27 20:21:47 +0300

    feat: add models for recipe and ingredient

---
+++ backend/recipes/models.py
@@ -0,0 +1,103 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+MIN_COOKING_VALUE = 1
+MAX_COOKING_VALUE = 15000
+MIN_AMOUNT_VALUE = 1
+MAX_AMOUNT_VALUE = 10000
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name="Название",
+    )
+    measurement_unit = models.CharField(
+        max_length=100,
+        verbose_name="Единица измерения",
+    )
+
+    class Meta:
+        verbose_name = "Ингредиент"
+        verbose_name_plural = "Ингредиенты"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Автор',
+    )
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название',
+    )
+    image = models.ImageField(
+        upload_to='recipes/images/',
+        blank=True,
+        verbose_name='Картинка',
+    )
+    text = models.TextField(
+        max_length=256,
+        verbose_name='Описание',
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='IngredientRecipe',
+        verbose_name='Ингредиенты',
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        validators=[
+            MinValueValidator(
+                MIN_COOKING_VALUE
+            ),
+            MaxValueValidator(
+                MAX_COOKING_VALUE
+            )
+        ],
+        verbose_name='Время приготовления',
+    )
+
+    class Meta:
+        ordering = ['-id']
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
+
+
+class IngredientRecipe(models.Model):
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        verbose_name='Ингредиент',
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        verbose_name='Рецепт',
+    )
+
+    amount = models.IntegerField(
+        validators=[
+            MinValueValidator(
+                MIN_AMOUNT_VALUE
+            ),
+            MaxValueValidator(
+                MAX_AMOUNT_VALUE
+            )
+        ]
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент в рецепте'
+        verbose_name_plural = 'Ингредиенты в рецепте'
