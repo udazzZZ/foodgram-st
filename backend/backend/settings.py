9abb58e962767647b46bf61e83fbab763221ce00
udazzZZ <pirogova.dv96@mail.ru>
2025-09-23 15:49:23 +0300

    feat: finish part2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -135,7 +135,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/app/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
