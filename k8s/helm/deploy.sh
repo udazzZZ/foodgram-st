8ee197d1d42154792c3e1ee4c32afbaa2a2f61a4
udazzZZ <pirogova.dv96@mail.ru>
2025-12-15 15:12:03 +0300

    feat: add redis and redis-insight

--- k8s/helm/deploy.sh
+++ k8s/helm/deploy.sh
@@ -7,3 +7,10 @@ set +a
 helm secrets --evaluate-templates --backend vals upgrade --install foodgram ./ \
     -n foodgram \
     -f values.yaml
+
+helm repo add heywood8-helm-charts https://heywood8.github.io/helm-charts/
+
+helm install redis-insight heywood8-helm-charts/redisinsight \
+    -n foodgram \
+    -f redis-insight.yaml \
+    --version 0.4.5
